4b57de56d82e958e5711e1879c7fd0e8ab5dd11d
0MINAMI0 <haolyu1125@gmail.com>
2022-09-01 16:28:39 +0800

    W

--- HW/HW1/HWq3/HW1_3.java
+++ HW/HW1/HWq3/HW1_3.java
@@ -1,19 +1,27 @@
 import java.util.*;
 public class HW1_3 {
+        public static int getTriangularNumber(int n){
+        return n;
+        }
         public static void main(String[] args) {
             int data = 0;
             int flag = 0;
             int i = 1;
-            System.out.println("The first 100 triangular numbers are:");
-            for (; i <= 100;i++){
+            Scanner input=new Scanner(System.in);
+            System.out.println("How many triangular number: ");
+            int num = input.nextInt();
+            System.out.println("The first" + num +" triangular numbers are:");
+            for (; i <= getTriangularNumber(num);i++){
                 data = data +i;
                 System.out.print(data + " ");
                 flag++;
-                if (flag == 5)
-                {
-                    System.out.print("\n");
-                    flag = 0;
-                }
+            if (flag == 5)
+            {
+                System.out.print("\n");
+                flag = 0;
             }
+            } 
+            
+            
        }
     }
