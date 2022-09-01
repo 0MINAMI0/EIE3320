209319c6c97de705e340eb7020c5d496fa1566ef
0MINAMI0 <haolyu1125@gmail.com>
2022-09-01 21:37:36 +0800

    W

---
+++ HW/HW1/HWq5/HW1_5.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class HW1_5
+{
+    public static void main(String[] args) {
+        System.out.print("Java Candy: ");
+        Scanner input = new Scanner(System.in);
+        int[] inputs = new int[2];
+        for(int i = 0; i < 2; i++)
+        {
+            inputs[i] = input.nextInt();
+        }
+        int n = inputs[0];
+        int m = inputs[1];
+        int candies = 0;
+        int tem = n;
+        //System.out.println(m);
+        //System.out.println(n);
+        while (tem/m != 0){
+            n = n + tem/m;
+            candies = n;
+            tem = tem/m;
+            System.out.println("tem/m: " + tem/m);
+            System.out.println("candies: " + candies);
+        }
+        System.out.println("n=" + inputs[0] + "; m=" + m + "; No. of candies=" + candies);
+    }
+}
