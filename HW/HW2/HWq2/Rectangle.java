e284ec9edb6abcfe86e3e025d5b6480eb9b176e3
0MINAMI0 <haolyu1125@gmail.com>
2022-09-02 20:27:58 +0800

    M

--- HW/HW2/HWq2/Rectangle.java
+++ HW/HW2/HWq2/Rectangle.java
@@ -1,11 +1,28 @@
+import java.util.*;
 public class Rectangle
 {
-    class creates_rectangle {
-        
+    public static double creates_rec(double width, double height) {
+        double area = width * height;
+        return area;
+    }
+    public double creates_default_rec() {
+        double width = 1;
+        double height = 1;        
+        return width * height;
     }
    public static void main(String[] args) {
-       double width = 1;
-       double height = 1;
+        System.out.print("Input the width and height: ");
+        Scanner input = new Scanner(System.in);
+        double[] inputs = new double[2];
+        for(int i = 0; i < 2; i++)
+        {
+            inputs[i] = input.nextInt();
+        }
+        double width = inputs[0];
+        double height = inputs[1];
+        
+        System.out.print("The area of a rectangle with width " + width + " and height " + height + " is " + creates_rec(width, height));
+
        
    }
 }
