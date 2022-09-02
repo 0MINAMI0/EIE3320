69cf95502451847c11edd6e88c488557801a08fe
0MINAMI0 <haolyu1125@gmail.com>
2022-09-02 13:35:11 +0800

    M

---
+++ HW/HW2/HWq2/Rectangle.java
@@ -0,0 +1,11 @@
+public class Rectangle
+{
+    class creates_rectangle {
+        
+    }
+   public static void main(String[] args) {
+       double width = 1;
+       double height = 1;
+       
+   }
+}
