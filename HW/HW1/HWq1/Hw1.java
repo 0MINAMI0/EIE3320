571bb7f67e00d910ac496540263de58644b74b8a
0MINAMI0 <haolyu1125@gmail.com>
2022-09-15 16:00:51 +0800

    W

--- HW/HW1/HWq1/Hw1.java
+++ HW/HW1/HWq1/Hw1.java
@@ -7,15 +7,15 @@ public class Hw1
             int rows = input.nextInt();
             System.out.println("Hello: LYU Hao 20042543d");      
             int i;
+            int current = rows;// current is current rows
             int index = 0;//index before 1
             int index2 = 0;//index start from 1 in the middle
-            int j = rows;// j is current rows
             int space = 0;
             for (i = 1; i <= rows; i++){
-                for ( space = 1; space < j; j--){
+                for ( space = 1; space < current; current--){
                     System.out.print("   ");
                 }
-                j = rows - i;
+                current = rows - i;
                 for ( index2 = i; 1 < index2; index2--){
                     if (index2 > 9)
                         System.out.print(" " + index2);
@@ -24,12 +24,12 @@ public class Hw1
                 }
                 
                 for ( index = 1; index < (i + 1); index++){
-                        if (index > 9)
+                        if (index > 10)
                         System.out.print(" " + index);
-                    else
+                        else
                         System.out.print("  " + index);
                 }
-                System.out.print("\n");
+                System.o    ut.print("\n");
             }
     }
 }
