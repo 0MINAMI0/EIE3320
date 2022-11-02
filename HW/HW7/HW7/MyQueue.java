919068d1d74fa0d47ac7cef43148c82ed537fb1b
0MINAMI0 <haolyu1125@gmail.com>
2022-11-02 08:19:09 +0800

    1

---
+++ HW/HW7/HW7/MyQueue.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class MyQueue here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MyQueue
+{
+    @Override
+ public String toString() {
+ return "stack: " + list.toString();
+ }
+}
---
+++ HW/HW7/HW7/MyStack.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class MyStack here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MyStack
+{
+ 
+    @Override
+ public String toString() {
+ return "stack: " + list.toString();
+ }
+}
