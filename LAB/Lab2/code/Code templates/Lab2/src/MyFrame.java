de2d42fd2e3931feac2edef79e55ad83a1195809
0MINAMI0 <haolyu1125@gmail.com>
2022-11-02 19:15:07 +0800

    LAB2

---
+++ LAB/Lab2/code/Code templates/Lab2/src/MyFrame.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+public class MyFrame {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Library Admin System");
+		frame.setSize(1000,600);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
+	}
+}
