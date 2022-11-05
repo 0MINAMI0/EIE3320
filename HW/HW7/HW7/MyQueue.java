4d639c57e53cf852d29f9ca160ad910d646295a3
0MINAMI0 <haolyu1125@gmail.com>
2022-11-05 19:38:28 +0800

    HW7

--- HW/HW7/HW7/MyQueue.java
+++ HW/HW7/HW7/MyQueue.java
@@ -1,22 +1,22 @@
 import java.util.*;
 public class MyQueue<E>
 {
-      private LinkedList<E> list 
-    = new LinkedList<E>();
+    private LinkedList<E> list = new LinkedList<E>();
     @Override
- public String toString() {
- return "stack: " + list.toString();}
- 
-  public void enqueue(E e) {
-	  list.addLast(e);
-  }
-  
+    public String toString() {
+        return "stack: " + list.toString();
+    }
+
+    public void enqueue(E e) {
+        list.addLast(e);
+    }
+
     public E dequeue() {
-	  return list.removeFirst();
-  }
-  
+        return list.removeFirst();
+    }
+
     public int getSize() {
-	  return list.size();
-  }
- }
+        return list.size();
+    }
+}
 
--- HW/HW7/HW7/MyStack.java
+++ HW/HW7/HW7/MyStack.java
@@ -1,10 +1,37 @@
 import java.util.*;
-public class MyStack
+public class MyStack<E>
 {
- List<E> list = new ArrayList<>();
- @Override
- public String toString() {
- return "stack: " + list.toString();
- 
- }
+    ArrayList<E> list = new ArrayList<>();
+    @Override
+    public String toString() {
+        return "stack: " + list.toString();
+
+    }
+
+    public int getSize() {
+        return list.size();
+    }
+
+    public E peek(){
+        int i = this.getSize();
+        return list.get(i-1);
+    }
+
+    public E pop(){
+        int i = this.getSize();
+        E tem = list.get(i-1);
+        list.remove(i-1);
+        return tem;
+    }
+    public void push(E o){
+        int i = list.size();
+        list.add(i,o);
+    }
+
+    public boolean isEmpty() {
+        if(list.isEmpty()){
+            return true;
+        }
+        else return false;
+    }
 }
